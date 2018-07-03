0fa61aab9b3c2b1738558e52c0c0a1c4b371f129
Pierre <pierre.gaxatte@gmail.com>
2018-07-03 07:43:03 +0200

    Simple JSON publishing

--- main.go
+++ main.go
@@ -2,19 +2,62 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"errors"
+	"fmt"
 	"log"
+	"os"
+	"os/signal"
 	"strconv"
 	"strings"
 
 	"github.com/tarm/serial"
+	"github.com/yosssi/gmq/mqtt/client"
 )
 
 type SensorData struct {
-	Manufacturer string
-	Id           uint16
-	Temperature  float32
-	Humidity     uint16
+	Model       string   `json:"model"`
+	Id          string   `json:"id"`
+	Temperature *float32 `json:"t,omitempty"`
+	Humidity    *uint16  `json:"h,omitempty"`
+}
+
+func (sd *SensorData) String() string {
+	format := "%s [%s]:"
+	args := []interface{}{
+		sd.Model,
+		sd.Id,
+	}
+
+	if sd.Temperature != nil {
+		format += " temp=%.1f°C"
+		args = append(args, *sd.Temperature)
+	}
+
+	if sd.Humidity != nil {
+		format += " hum=%d%%"
+		args = append(args, *sd.Humidity)
+	}
+
+	return fmt.Sprintf(format, args...)
+}
+
+func (sd *SensorData) Publish(cli *client.Client) (err error) {
+	b, err := json.Marshal(sd)
+	if err != nil {
+		return err
+	}
+	log.Print(string(b))
+
+	err = cli.Publish(&client.PublishOptions{
+		TopicName: []byte("rflink"),
+		Message:   b,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func strToUint16(s string, base int) (uint16, error) {
@@ -26,42 +69,35 @@ func strToUint16(s string, base int) (uint16, error) {
 }
 
 func parse(msg string) (*SensorData, error) {
-	log.Printf(msg)
 	pieces := strings.Split(msg, ";")
 
 	sd := SensorData{
-		Manufacturer: pieces[2],
+		Model: strings.Replace(pieces[2], " ", "_", -1),
 	}
 	for i := 3; i < len(pieces); i++ {
 		arr := strings.SplitN(pieces[i], "=", 2)
 		switch arr[0] {
 		case "ID":
-			id, err := strToUint16(arr[1], 16)
-			if err != nil {
-				return nil, errors.New("Skipping message, id could not be parsed")
-			}
-			//log.Printf("ID: %d (%s)", id, arr[1])
-			sd.Id = id
+			sd.Id = arr[1]
 		case "TEMP":
 			t, err := strToUint16(arr[1], 16)
 			if err != nil {
 				return nil, errors.New("Skipping message, temperature could not be parsed")
 			}
-			//log.Printf("Temperature: %d (%s)", t, arr[1])
-			sd.Temperature = float32(t) / 10
+			temp := float32(t) / 10
+			sd.Temperature = &temp
 		case "HUM":
 			h, err := strToUint16(arr[1], 10)
 			if err != nil {
 				return nil, errors.New("Skipping message, humidity could not be parsed")
 			}
-			//log.Printf("Humidity: %d (%s)", h, arr[1])
-			sd.Humidity = h
+			sd.Humidity = &h
 		}
 	}
 	return &sd, nil
 }
 
-func read(r *bufio.Reader) {
+func read(r *bufio.Reader, cli *client.Client) {
 	for {
 		line, _, err := r.ReadLine()
 		if err != nil {
@@ -73,11 +109,40 @@ func read(r *bufio.Reader) {
 			log.Printf("Error parsing \"%s\": %s", line, err)
 		}
 
-		log.Printf("DATA: %+v", sd)
+		err = sd.Publish(cli)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 }
 
 func main() {
+	// Set up channel on which to send signal notifications.
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc, os.Interrupt, os.Kill)
+
+	/*
+	 * Setup the MQTT client
+	 */
+	cli := client.New(&client.Options{
+		ErrorHandler: func(err error) {
+			log.Fatal(err)
+		},
+	})
+	defer cli.Terminate()
+
+	err := cli.Connect(&client.ConnectOptions{
+		Network:  "tcp",
+		Address:  "10.1.0.4:1883",
+		ClientID: []byte("rflink"),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	/*
+	 * Setup the serial connection and start the reader
+	 */
 	c := &serial.Config{Name: "/dev/ttyUSB0", Baud: 57600}
 	s, err := serial.OpenPort(c)
 	if err != nil {
@@ -86,5 +151,15 @@ func main() {
 	defer s.Close()
 
 	r := bufio.NewReader(s)
-	read(r)
+	go read(r, cli)
+
+	/*
+	 * Wait for interruption
+	 */
+	<-sigc
+
+	// Disconnect the Network Connection.
+	if err := cli.Disconnect(); err != nil {
+		log.Fatal(err)
+	}
 }
