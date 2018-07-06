bc49e4a983982a3856cc865f3dd17b3d02206bbb
Pierre <pierre.gaxatte@gmail.com>
2018-07-06 21:22:10 +0200

    Add logging and version

--- main.go
+++ main.go
@@ -8,13 +8,18 @@ import (
 	"github.com/lilorox/go-rflink/rflink"
 )
 
+var VERSION = "0.0.1"
+
 func main() {
+	log.Printf("Starting go-rflink v%s", VERSION)
+
 	// Set up channel on which to send signal notifications.
 	sigc := make(chan os.Signal, 1)
 	signal.Notify(sigc, os.Interrupt, os.Kill)
 
 	// Parse options
 	opts := rflink.ParseOptions()
+	log.Printf("Options: %v", opts)
 
 	// Setup the MQTT publisher
 	p, err := rflink.NewPublisher(opts)
@@ -22,6 +27,7 @@ func main() {
 		log.Fatal(err)
 	}
 	defer p.Terminate()
+	log.Print("MQTT publisher created")
 
 	// Setup the sensor reader
 	sr, err := rflink.NewSensorReader(opts)
@@ -29,6 +35,7 @@ func main() {
 		log.Fatal(err)
 	}
 	defer sr.Close()
+	log.Print("Sensor reader created")
 
 	// Start reading/publishing loop
 	p.SensorInput = sr
@@ -36,9 +43,11 @@ func main() {
 
 	// Wait for interruption
 	<-sigc
+	log.Print("Interruption signal received")
 
 	// Disconnect the Network Connection.
 	if err := p.Disconnect(); err != nil {
 		log.Fatal(err)
 	}
+	log.Print("The End.")
 }
